bd06a055de9f670270cbb5bcfe1556ff99ab4576
w0rm29 <adi.maheshwari98@gmail.com>
2023-10-02 22:10:03 -0400

    Day3

--- numIdenticalPairs.py
+++ numIdenticalPairs.py
@@ -9,6 +9,13 @@ Example:
     Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 """
 
+##########################
+# Solution 1
+##########################
+
+from typing import List
+
+
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         count = 0
@@ -16,4 +23,21 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] == nums[j]:
                     count += 1
+        return count
+    
+########################
+# Solution 2
+########################
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        dic = {}
+        count = 0
+
+        for num in nums:
+            if num not in dic:
+                dic[num] = 0
+            else:
+                dic[num] += 1
+                count += dic[num]
         return count
