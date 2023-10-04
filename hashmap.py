e376585dbf932aee998a88ae6c7cd5bfe913d035
w0rm29 <adi.maheshwari98@gmail.com>
2023-10-04 09:18:56 -0400

    Day4

--- hashmap.py
+++ hashmap.py
@@ -26,7 +26,7 @@ myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2
 myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
 myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
 myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
-myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]`
 """
 
 class MyHashMap:
